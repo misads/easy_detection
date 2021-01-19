61a03a875f2448b07ea48b3aa9b98b1325fc1d98
misads <523131316@qq.com>
2021-01-19 18:07:28 +0800

    install.sh

---
+++ install.sh
@@ -0,0 +1,20 @@
+# 需要先自行安装pytorch和torchvision
+pip install ipdb
+pip install numpy
+pip install tensorboardX
+pip install utils-misc
+pip install mscv
+pip install matplotlib
+pip install opencv-python==4.2.0.34 
+pip install opencv-python-headless==4.2.0.34
+pip install albumentations==0.5.1 
+pip install scikit-image
+pip install easydict
+pip install timm==0.1.30  # timm >= 0.2.0 不兼容 
+pip install typing_extensions
+pip install tqdm
+pip install PyYAML
+pip install Cython
+pip install pycocotools  # 需要Cython
+pip install omegaconf  # effdet依赖
+
