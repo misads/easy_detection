1a569a069831cd8ebc4805b554b880bd4f5ddd1c
misads <523131316@qq.com>
2021-07-06 20:17:25 +0800

    refactor

--- dataloader/dataloaders.py
+++ dataloader/dataloaders.py
@@ -1,46 +1,9 @@
 # encoding=utf-8
 from torch.utils.data import DataLoader
-from torchvision import datasets, transforms
-# from configs.data_roots import get_dataset
-from configs.transforms import get_transform
-
-from dataloader.voc import VOCTrainValDataset
-from dataloader.coco import CocoDataset
 from dataloader.list_dataset import get_all_datasets
-import albumentations as A
-from albumentations.pytorch.transforms import ToTensorV2
 
 from options import opt, config
-from mscv.image import tensor2im
 import torch
-import pdb
-
-# dataset = get_dataset(config.DATA.DATASET)
-# d = dataset()
-
-# if 'TRANSFORM' not in config.DATA:
-#     config.DATA.TRANSFORM = None
-
-# transform = get_transform(config.DATA.TRANSFORM)
-# t = transform(config)
-
-# dataset_variables = ['voc_root', 'train_split', 'val_split', 'class_names', 'img_format', 'data_format']
-
-# transform_variables = ['width', 'height', 'train_transform', 'val_transform']
-
-# for v in dataset_variables:
-#     # 等价于 exec(f'{v}=d.{v}')
-#     if hasattr(d, v):
-#         locals()[v] = getattr(d, v)  # 把类的成员变量赋值给当前的局部变量
-
-# for v in transform_variables:
-#     if hasattr(t, v):
-#         locals()[v] = getattr(t, v)  # 把类的成员变量赋值给当前的局部变量
-
-
-# config.DATA.CLASS_NAMES = class_names
-# config.DATA.NUM_CLASSESS = len(class_names)
-
 
 def collate_fn(batch):
     target = {}
@@ -56,35 +19,10 @@ def collate_fn(batch):
                                 batch[i]['yolo5_boxes']], 1) for i in range(b)], 0)
     return target
 
-"""
-Datasets
-"""
-# if data_format == 'VOC':
-#     if hasattr(d, 'train_split'):
-#         train_dataset = VOCTrainValDataset(voc_root, 
-#                 class_names,
-#                 split=train_split,
-#                 format=img_format,
-#                 transforms=train_transform)
-
-#     if hasattr(d, 'val_split'):
-#         val_dataset = VOCTrainValDataset(voc_root,
-#                 class_names,
-#                 split=val_split,
-#                 format=img_format,
-#                 transforms=val_transform)
-
-# elif data_format == 'COCO':
-#     if hasattr(d, 'train_split'):
-#         train_dataset = CocoDataset(voc_root, train_split, transforms=train_transform)
-
-#     if hasattr(d, 'val_split'):
-#         val_dataset = CocoDataset(voc_root, val_split, transforms=val_transform)
-
+# Datasets
 train_dataset, val_dataset = get_all_datasets()
-"""
-Dataloaders
-"""
+
+# Dataloaders
 if train_dataset is None:
     train_dataloader = None
 else:
@@ -105,24 +43,3 @@ else:
         batch_size=config.OPTIMIZE.BATCH_SIZE,
         num_workers=config.MISC.NUM_WORKERS // 2,
         drop_last=False)
-
-
-# if hasattr(d, 'train_split'):
-#     train_dataloader = torch.utils.data.DataLoader(train_dataset,
-#         shuffle=True,
-#         collate_fn=collate_fn,
-#         batch_size=config.OPTIMIZE.BATCH_SIZE,
-#         num_workers=config.MISC.NUM_WORKERS,
-#         drop_last=True)
-# else:
-#     train_dataloader = None
-
-# if hasattr(d, 'val_split'):
-#     val_dataloader = torch.utils.data.DataLoader(val_dataset,
-#         shuffle=False,
-#         collate_fn=collate_fn,
-#         batch_size=config.OPTIMIZE.BATCH_SIZE,
-#         num_workers=config.MISC.NUM_WORKERS // 2,
-#         drop_last=False)
-# else:
-#     val_dataloader = None
